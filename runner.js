970b0a922f1b20583f4946b5f6458c3f75c2b67b
Jean Moreira <jean.biomedicina@gmail.com>
2023-03-12 18:49:07 -0300

    Integrando testes com tesult

---
+++ runner.js
@@ -0,0 +1,17 @@
+const cypress = require('cypress')
+const tesults = require('cypress-tesults-reporter');
+
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjdkYmJkZDBiLTU4ZGItNDYyNC1hN2ZhLThhOWUzZDcxZWU3ZC0xNjc4NjU1NDE1MjYxIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiZTk5NTY5ZDAtMzJiYS00OWI0LTgyZTYtMzU4ZGQxY2I1ZmZlIiwidHlwZSI6InQifQ.p5mic75Nvf0tAhRfjzaWKMZfFN7o0Tv4ncK22LQVTvo'
+
+cypress.run({
+  // specs to run here
+})
+.then((results) => {
+  const args = {
+    target: TOEKN,
+  }
+  tesults.results(results, args);
+})
+.catch((err) => {
+ console.error(err)
+})
